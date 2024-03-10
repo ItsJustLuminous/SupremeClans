3c94f1e6f9afd3d834ad9826c61ba49a2c43284d
ItsJustLuminous <callumcradds07@outlook.com>
2024-03-10 09:44:12 +0000

    Files from inteliji.

---
+++ src/main/java/com/callum/supremeclans/SupremeClans.java
@@ -0,0 +1,17 @@
+package com.callum.supremeclans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SupremeClans extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
